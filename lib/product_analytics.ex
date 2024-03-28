45ec6d7dd626224169b9fc1bf04a9ccc4f66cc4c
ArunMuthuram <arunramgt@gmail.com>
2024-03-28 07:43:47 +0530

    initial_commit

---
+++ lib/product_analytics.ex
@@ -0,0 +1,18 @@
+defmodule ProductAnalytics do
+  @moduledoc """
+  Documentation for `ProductAnalytics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProductAnalytics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/product_analytics_test.exs
@@ -0,0 +1,8 @@
+defmodule ProductAnalyticsTest do
+  use ExUnit.Case
+  doctest ProductAnalytics
+
+  test "greets the world" do
+    assert ProductAnalytics.hello() == :world
+  end
+end
